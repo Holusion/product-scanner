569a4af03be4d54b0e1698b873a9f46892b948b7
Sebastien Dumetz <s.dumetz@holusion.com>
2018-12-13 14:19:43 +0100

    add some test for Node objects

--- test/Node_test.js
+++ test/Node_test.js
@@ -17,3 +17,33 @@ test("Node.mapAddresses()",function(t){
   t.deepEqual(addrs, ["192.168.1.1","2001:0db8:0000:85a3:0000:0000:ac1f:8001%eth0"]);
   t.end();
 })
+
+test("Node.merge()",function(t){
+  t.test("new node overwrite values",function(t){
+    const n1 = new Node({name: "toto", foo:"something"})
+    const n2 = new Node({name: "toto", foo:"something_else"})
+    n1.merge(n2);
+    t.deepEqual(n1, n2);
+    t.end();
+  });
+  t.test("keep special values",function(t){
+    const n1 = new Node({name: "toto", bar:"foobar"})
+    const n2 = new Node({name: "toto"})
+    n1.merge(n2);
+    t.equal(n1.bar, "foobar");
+    t.end();
+  })
+  t.test("can merge invalid values",function(t){
+    const n1 = new Node({name: "toto", bar:"foobar"})
+    n1.merge(null);
+    t.deepEqual(n1, new Node({name: "toto", bar:"foobar"}));
+    t.end();
+  })
+  t.test("can merge invalid values",function(t){
+    const n1 = new Node({name: "toto", bar:"foobar"})
+    n1.merge(null);
+    t.deepEqual(n1, new Node({name: "toto", bar:"foobar"}));
+    t.end();
+  })
+  t.end();
+})
